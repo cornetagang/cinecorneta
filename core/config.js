b709143bb73687ed6a7db46693dfa308a0fe76b5
cornetagang <benja104lokito@gmail.com>
2026-03-18 16:16:40 -0300

    Update config.js

--- core/config.js
+++ core/config.js
@@ -19,7 +19,7 @@ export const firebaseConfig = {
  * URLs de API
  */
 export const API_URL = {
-    BASE_URL: 'https://script.google.com/macros/s/AKfycbx9pTTPbBj2GPIeUKmXDbmvxdiAgIjOJwhQI_XMGr4G-PVn29TIRqR7pn9CM-5sMGw2/exec',
+    BASE_URL: 'https://script.google.com/macros/s/AKfycby9S3S2GOc4aQcKGg-6ULAJF50flFbufdgZRkb2NJ076HPuJvppl22tGW1brl4fFZ4e/exec',
     
     endpoints: {
         series: 'series',
